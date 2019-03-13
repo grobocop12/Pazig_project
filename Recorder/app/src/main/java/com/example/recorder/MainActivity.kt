3874b29123661948b5a8e16e84e57396aeb32819
grobocop12 <chasnik97876@gmail.com>
2019-03-13 19:00:27 +0100

    added permission setup

--- Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -2,31 +2,42 @@ package com.example.recorder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var recorder : MediaRecorder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        setupPermissions()
+        recorder = MediaRecorder()
+        Log.i("recorder",recorder.activeMicrophones.toString())
     }
 
-    private fun setupPermission(){
-        val permission = ContextCompat.checkSelfPermission(this,
+    private fun setupPermissions(){
+        val microphonePermission = ContextCompat.checkSelfPermission(this,
             Manifest.permission.RECORD_AUDIO)
 
-        if(permission != PackageManager.PERMISSION_GRANTED){
-            Log.i("Permission","Permission to record denied")
+
+        val writePermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if(microphonePermission != PackageManager.PERMISSION_GRANTED || writePermission != PackageManager.PERMISSION_GRANTED){
+            //Log.i("Permission","Permission to record denied")
             makeRequest()
         }
     }
 
     private fun makeRequest(){
-
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            101)
     }
 }
