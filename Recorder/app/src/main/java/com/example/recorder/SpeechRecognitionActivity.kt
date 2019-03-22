b47e1c149af63181449a4f9b8ffc28ffb7494a58
grobocop <chasnik97876@gmail.com>
2019-03-23 00:39:32 +0100

    recognizer moved to another activity

---
+++ Recorder/app/src/main/java/com/example/recorder/SpeechRecognitionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recorder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeechRecognitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speech_recognition)
+    }
+}
